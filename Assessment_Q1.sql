aecc3a8c2329f7ee29def66c9d41d01cd943ff44
Lawalson olatomiwa <108265855+LAWALSON-T@users.noreply.github.com>
2025-05-19 09:15:52 +0100

    Update Assessment_Q1.sql

--- Assessment_Q1.sql
+++ Assessment_Q1.sql
@@ -1,21 +1,22 @@
 -- Calculate customers with both savings and investment plans, showing counts and total deposits
-
 WITH savings AS (
     SELECT s.owner_id,
            COUNT(DISTINCT s.plan_id) AS savings_count,
-           SUM(s.confirmed_amount) AS savings_total  -- total amount in kobo
+           SUM(s.confirmed_amount) AS savings_total
     FROM savings_savingsaccount s
     JOIN plans_plan p ON s.plan_id = p.id
     WHERE p.is_regular_savings = 1
+    AND s.confirmed_amount > 0
     GROUP BY s.owner_id
 ),
 investments AS (
     SELECT s.owner_id,
            COUNT(DISTINCT s.plan_id) AS investment_count,
-           SUM(s.confirmed_amount) AS investment_total  -- total amount in kobo
+           SUM(s.confirmed_amount) AS investment_total
     FROM savings_savingsaccount s
     JOIN plans_plan p ON s.plan_id = p.id
     WHERE p.is_a_fund = 1
+    AND s.confirmed_amount > 0
     GROUP BY s.owner_id
 )
 SELECT
@@ -23,7 +24,7 @@ SELECT
     CONCAT(u.first_name, ' ', u.last_name) AS name,
     s.savings_count,
     i.investment_count,
-    ROUND((s.savings_total + i.investment_total) / 100, 2) AS total_deposits --convert kobo to naira
+    ROUND((s.savings_total + i.investment_total) / 100, 2) AS total_deposits -- convert to naira
 FROM savings s
 JOIN investments i ON s.owner_id = i.owner_id
 JOIN users_customuser u ON u.id = s.owner_id
